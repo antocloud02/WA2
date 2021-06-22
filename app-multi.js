87879d22a49229e54b93982115f4a8a8867b5e56
antocloud02 <antocloud02@gmail.com>
2021-06-22 11:01:23 +0800

    barulagi

--- app-multi.js
+++ app-multi.js
@@ -263,7 +263,7 @@ app.post("/download-file", async (req, res) => {
   });
 });
 // Send message
-app.post("/send-message", (req, res) => {
+app.post("/send-message", async (req, res) => {
   const sender = req.body.sender;
   const number = phoneNumberFormatter(req.body.number);
   const message = req.body.message;
